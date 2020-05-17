523af67fdd05a28ba93da85a076e67e0e82c079a
Chris Sewell <chrisj_sewell@hotmail.com>
2020-05-17 11:28:07 +0100

    Use switch to sphinx_book_theme

--- docs/conf.py
+++ docs/conf.py
@@ -74,13 +74,16 @@ pygments_style = "sphinx"
 # a list of builtin themes.
 #
 # html_theme = "alabaster"
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
+panels_add_boostrap_css = False
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "single_page": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
